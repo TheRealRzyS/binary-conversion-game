998b99eb20a0e4cab86de51fb263e016e39115c8
TheRealRzyS <62859857+TheRealRzyS@users.noreply.github.com>
2023-12-30 00:40:15 +0800

    DecimaltoBinaryGame.c

--- DecimaltoBinaryGame.c
+++ DecimaltoBinaryGame.c
@@ -1,63 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> // Include the time.h header for the time limit function in the game
-#include <math.h> // Include the math.h header for pow() function
-#include <string.h> // Include the string.h header for strcmp() function
+#include <time.h>
+#include <math.h>
+#include <string.h>
 
-#define MAX_QUESTIONS 100 // Increase the number of questions
-#define TIME_LIMIT_SECONDS 120 // Set the time limit in seconds
-#define NUM_DIFFICULTY_LEVELS 3 // Number of difficulty levels
+#define MAX_QUESTIONS 100
+#define TIME_LIMIT_SECONDS 120
+#define NUM_DIFFICULTY_LEVELS 3
+#define BUFFER_SIZE 1024
 
-const int MAX_DIGITS[] = {1, 2, 4}; // Array for maximum digits for each difficulty level
+const int MAX_DIGITS[] = {1, 2, 4};
 
-// Function to generate a random number with up to the specified number of digits
+// Generates a random number with up to the specified number of digits
 int generateRandomNumber(int maxDigits) {
-    int max_value = pow(10, maxDigits) - 1; // Calculate the maximum value using pow() function
-    if (max_value > 1024) {
-        max_value = 1024; // Limit the maximum value to 1024
-    }
-    return rand() % max_value; // Generate random numbers with up to the specified number of digits
+    int max_value = (int) pow(10, maxDigits) - 1;
+    max_value = (max_value > 1024) ? 1024 : max_value;
+    return rand() % (max_value + 1);
 }
 
-
-// Function to convert a decimal number to binary
+// Converts a decimal number to binary
 int decimalToBinary(int decimal) {
     int binary = 0, base = 1;
-
     while (decimal > 0) {
         binary += (decimal % 2) * base;
         decimal >>= 1;
         base *= 10;
     }
-
     return binary;
 }
 
-// Function to start the game
+// Validates if input is a binary number
+int isBinary(const char *str) {
+    while (*str) {
+        if (*str != '0' && *str != '1') return 0;
+        str++;
+    }
+    return 1;
+}
+
+// Starts the game with the given maximum digits
 void startGame(int maxDigits) {
     srand(time(0));
     int score = 0;
-    time_t startTime = time(NULL); // Get the current time
-
+    time_t startTime = time(NULL);
     printf("You have 2 minutes to answer as many questions as possible.\n");
 
     while ((time(NULL) - startTime) < TIME_LIMIT_SECONDS) {
         int decimal = generateRandomNumber(maxDigits);
         int correctBinary = decimalToBinary(decimal);
-        int userBinary;
+        char userBinary[BUFFER_SIZE];
 
         printf("Convert %d to binary: ", decimal);
+        fgets(userBinary, BUFFER_SIZE, stdin);
+        userBinary[strcspn(userBinary, "\n")] = 0; // Remove newline character
 
-        // Validate user's input
-        if (scanf("%d", &userBinary) != 1) {
-            printf("Invalid input. Please enter a number.\n");
-            while (getchar() != '\n'); // Clear the input buffer
+        if (!isBinary(userBinary)) {
+            printf("Invalid input. Please enter a binary number.\n");
             continue;
         }
 
-        while (getchar() != '\n'); // Clear the input buffer
-
-        if (userBinary == correctBinary) {
+        int userBinaryInt = strtol(userBinary, NULL, 2);
+        if (userBinaryInt == correctBinary) {
             printf("Correct!\n");
             score++;
         } else {
@@ -70,39 +73,32 @@ void startGame(int maxDigits) {
 
 // Main function
 int main() {
+    char choiceStr[BUFFER_SIZE];
     int choice;
-    char quit[5];
+    char quit[BUFFER_SIZE];
 
     do {
-        printf("Binary Conversion Game\n");
-        printf("Choose a difficulty level:\n");
-        printf("1. Easy\n");
-        printf("2. Medium\n");
-        printf("3. Hard\n");
-        
+        printf("Binary Conversion Game\nChoose a difficulty level:\n1. Easy\n2. Medium\n3. Hard\n");
+
         do {
             printf("Choose your difficulty (1-3): ");
-            int status = scanf("%d", &choice);
-            while(getchar() != '\n'); // Clear the input buffer
-            if (status != 1) {
-                printf("Invalid input. Please enter a number.\n");
-            } else if (choice < 1 || choice > NUM_DIFFICULTY_LEVELS) {
+            fgets(choiceStr, BUFFER_SIZE, stdin);
+            choice = atoi(choiceStr);
+            if (choice < 1 || choice > NUM_DIFFICULTY_LEVELS) {
                 printf("Invalid choice. Please enter a number between 1 and 3.\n");
             }
         } while (choice < 1 || choice > NUM_DIFFICULTY_LEVELS);
 
-        int maxDigits = MAX_DIGITS[choice - 1]; // Use the array to set the maximum digits for the selected difficulty level
-
-        startGame(maxDigits);
+        startGame(MAX_DIGITS[choice - 1]);
 
         do {
             printf("Do you want to play again? (yes/no): ");
-            scanf("%4s", quit); // Read only the first 4 characters of the user's input
+            fgets(quit, sizeof(quit), stdin);
+            quit[strcspn(quit, "\n")] = 0; // Remove newline character
             if (strcmp(quit, "yes") != 0 && strcmp(quit, "no") != 0) {
                 printf("Invalid input. Please enter 'yes' or 'no'.\n");
             }
         } while (strcmp(quit, "yes") != 0 && strcmp(quit, "no") != 0);
     } while (strcmp(quit, "no") != 0);
 
-    return 0;
-}
+   
